da54942f3b3c54f6da4360fac429607f5fd3118b
aribaather <aribaather46@gmail.com>
2025-02-19 21:12:41 +0530

    removed unused code

--- src/config/database.ts
+++ src/config/database.ts
@@ -11,30 +11,6 @@ import { Sequelize } from "sequelize";
 //   }
 // );
 
-// export const sequelize = new Sequelize(
-//   process.env.DB_NAME || "dev_qe_db",
-//   process.env.DB_USER || "dev_qe_db_user",
-//   process.env.DB_PASSWORD || "t2Vrzoua5Gmjc2FHBMypKAR2yDYfnQcA",
-//   {
-//     host: process.env.DB_HOST || "dpg-cuqv99rv2p9s73fk9ltg-a.oregon-postgres.render.com",
-//     dialect: "postgres",
-//     logging: false,
-//   }
-// );
-
-// export const sequelize = new Sequelize(
-//   "jdbc:postgresql://dpg-cuqv99rv2p9s73fk9ltg-a.oregon-postgres.render.com:5432/dev_qe_db",
-//   {
-//     dialect: "postgres",
-//     protocol: "postgres",
-//     logging: false,
-//   }
-// );
-
-// export const sequelize = new Sequelize(
-//   "postgresql://dev_qe_db_user:t2Vrzoua5Gmjc2FHBMypKAR2yDYfnQcA@dpg-cuqv99rv2p9s73fk9ltg-a.oregon-postgres.render.com/dev_qe_db"
-// ); // Example for postgres
-
 export const sequelize = new Sequelize(
   "postgresql://dev_qe_db_user:t2Vrzoua5Gmjc2FHBMypKAR2yDYfnQcA@dpg-cuqv99rv2p9s73fk9ltg-a.oregon-postgres.render.com/dev_qe_db",
   {
