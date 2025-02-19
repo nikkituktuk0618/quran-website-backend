0b2c25fa136cd3a20b91726a0c69986e36ed9e83
aribaather <aribaather46@gmail.com>
2025-02-19 21:22:37 +0530

    added a cron job

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { sequelize } from "./config/database";
 import routes from "./routes";
 import { User, Course, Playlist, Video, Enrollment, Payment } from "./models";
+const cron = require("node-cron");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +14,18 @@ app.use(express.json());
 // Routes
 app.use("/api/v1", routes);
 
+cron.schedule("*/1 * * * *", async () => {
+  console.log("Executing API call at:", new Date().toISOString());
+
+  try {
+    const response = await axios.get(
+      "https://quran-website-backend.onrender.com/api/v1/ping"
+    );
+    console.log("API Response:", response.data);
+  } catch (error: any) {
+    console.error("Error calling API:", error.message);
+  }
+});
 // Database Connection
 (async () => {
   try {
