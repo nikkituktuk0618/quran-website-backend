398c9f4779383735d99772e158fef4cbe391449b
aribaather <aribaather46@gmail.com>
2025-03-11 01:55:45 +0530

    added constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,3 +3,7 @@ export const JWT_SECRET_KEY =
 
 export const razorpay_token_id = "rzp_test_srX1AYjdNKDTm0";
 export const RAZORPAY_KEY_SECRET = "CtQnYHRHj6G9mXgKF9KenGw9";
+export const Reset_password_FE_link =
+  "https://yourfrontend.com/reset-password?token=";
+export const company_email = "alfaizofficial@gmail.com";
+export const company_password = "gquv cdww yvpo zfip";
